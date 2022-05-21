ff56aac27127dd4fb487173564a99ad7e26d1fbe
Minh Nguyen <msminhnt@gmail.com>
2022-05-21 09:31:15 +0700

    Lab_2

--- src/lab_02/BMI.java
+++ src/lab_02/BMI.java
@@ -17,26 +17,29 @@ public class BMI {
 
             System.out.println("Underweight!");
             double bmiIncrease = 18.5 - bmi;
+            System.out.println(bmi);
             double weighIncrease = bmiIncrease * height *2;
-            System.out.println("You should increase at least: "+weighIncrease);
+            System.out.println("You should increase at least: "+weighIncrease + " "+ "kilogram");
 
         } else if (bmi <= 24.9) {
-
+            System.out.println(bmi);
             System.out.println("Normal weight!");
 
         } else if (bmi <= 29.9) {
 
             System.out.println("Overweight");
             double bmiDecrease = 29.5 - bmi;
+            System.out.println(bmi);
             double weighDecrease = bmiDecrease * height *2;
-            System.out.println("You should decrease approximate: "+weighDecrease);
+            System.out.println("You should decrease approximate: "+weighDecrease +" "+ "kilogram");
 
         }else {
 
             System.out.println("Obesity!!!");
             double bmiDec = bmi - 30;
+            System.out.println(bmi);
             double weighDec =  bmiDec* height *2;
-            System.out.println("You should decrease at least: "+weighDec);
+            System.out.println("You should decrease at least: "+weighDec +" "+ "kilogram");
         }
     }
 }
