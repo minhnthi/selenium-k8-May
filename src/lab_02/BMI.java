f5371883056d30cd4761283e76153b5367b225c1
Minh Nguyen <msminhnt@gmail.com>
2022-05-25 19:01:27 +0700

    Lab_2

--- src/lab_02/BMI.java
+++ src/lab_02/BMI.java
@@ -6,8 +6,10 @@ public class BMI {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("Please input height: ");
         float height = scanner.nextFloat();
+
         System.out.println("Please input weight: ");
         float weight = scanner.nextFloat();
 
@@ -16,29 +18,25 @@ public class BMI {
         if (bmi <= 18.5){
 
             System.out.println("Underweight!");
-            double bmiIncrease = 18.5 - bmi;
-            System.out.println(bmi);
-            double weighIncrease = bmiIncrease * height *2;
+            float bmiIncrease = 18.5F - bmi;
+            float weighIncrease = bmiIncrease * height *2;
             System.out.println("You should increase at least: "+weighIncrease + " "+ "kilogram");
 
         } else if (bmi <= 24.9) {
-            System.out.println(bmi);
             System.out.println("Normal weight!");
 
         } else if (bmi <= 29.9) {
 
             System.out.println("Overweight");
-            double bmiDecrease = 29.5 - bmi;
-            System.out.println(bmi);
-            double weighDecrease = bmiDecrease * height *2;
+            float bmiDecrease = 29.5F - bmi;
+            float weighDecrease = bmiDecrease * height *2;
             System.out.println("You should decrease approximate: "+weighDecrease +" "+ "kilogram");
 
         }else {
 
             System.out.println("Obesity!!!");
-            double bmiDec = bmi - 30;
-            System.out.println(bmi);
-            double weighDec =  bmiDec* height *2;
+            float bmiDec = bmi - 30.0F;
+            float weighDec =  bmiDec* height *2;
             System.out.println("You should decrease at least: "+weighDec +" "+ "kilogram");
         }
     }
