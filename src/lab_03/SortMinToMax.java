a4bf99da87bd2ac31c1e0fcc847b82d0a6fe6775
Minh Nguyen <msminhnt@gmail.com>
2022-06-02 23:25:04 +0700

    Lab03

--- src/lab_03/SortMinToMax.java
+++ src/lab_03/SortMinToMax.java
@@ -1,5 +1,6 @@
 package lab_03;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class SortMinToMax {
@@ -7,19 +8,23 @@ public class SortMinToMax {
     public static void main(String[] args) {
 
         int[] intArr = {12, 34, 1, 16, 28};
-        int tempSort;
-        for (int i = 0; i < intArr.length; i++) {
-            for (int j = i + 1; j < intArr.length; j++) {
-                if (intArr[i] > intArr[j]) {
-                    tempSort = intArr[j];
-                    intArr[j] = intArr[i];
-                    intArr[i] = tempSort;
+        int arrLength = intArr.length;
+        System.out.println("This is the initial integer array:");
+        System.out.println(Arrays.toString(intArr));
+        for (int unSortedIndex = arrLength - 1; unSortedIndex > 0; unSortedIndex--) {
+            for (int index = 0; index < unSortedIndex; index++) {
+                if (intArr[index] > intArr[index + 1]) {
+//                    int tmp = intArr[index];
+//                    intArr[index] = intArr[index+1];
+//                    intArr[index +1] = tmp;
+
+                    intArr[index] = intArr[index] + intArr[index + 1];
+                    intArr[index + 1] = intArr[index] - intArr[index + 1];
+                    intArr[index] = intArr[index] - intArr[index + 1];
                 }
             }
         }
-        System.out.println("The sort integer array from min to max is:");
-        for (int i = 0; i < intArr.length; i++) {
-            System.out.println(intArr[i]);
-        }
+        System.out.println("The sort integer array form Min to Max is: ");
+        System.out.println(Arrays.toString(intArr));
     }
 }
