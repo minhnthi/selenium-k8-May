90274b173203518633cec9671de8b61ec1bc438d
Minh Nguyen <msminhnt@gmail.com>
2022-06-15 22:34:43 +0700

    Lab04 Updated

--- src/lab_04/CreateMenu.java
+++ src/lab_04/CreateMenu.java
@@ -20,55 +20,63 @@ public class CreateMenu {
         boolean isContinuing = true;
         while (isContinuing) {
             System.out.println("==== MENU ====");
+
             System.out.println("1. Print all numbers\n ");
             System.out.println("2. Print maximum value\n");
             System.out.println("3. Print minimum value\n");
             System.out.println("4. Search number\n");
+            System.out.println("0. Exit\n ");
 
             Scanner scanner = new Scanner(System.in);
             System.out.println("Please select option:");
             int option = scanner.nextInt();
 
-            if (option == 0) {
-                isContinuing = false;
-            } else if (option == 1) {
-                System.out.println("All the numbers that we have in the array list:");
-                for (int index = 0; index < arrLength; index++) {
-                    System.out.println(arrNumber[index]);
-                }
-            } else if (option == 2 || option == 3) {
-                int min = arrNumber[0];
-                int max = arrNumber[0];
-                for (int index = 0; index < arrLength; index++) {
-                    if (arrNumber[index] < min) {
-                        min = arrNumber[index];
-                    } else if (arrNumber[index] > max) {
-                        max = arrNumber[index];
+            switch (option) {
+
+                case 0: isContinuing =false;
+                break;
+
+                case 1:
+                    System.out.println("All the numbers that we have in the array list:");
+                    for (int index = 0; index < arrLength; index++) {
+                        System.out.println(arrNumber[index]);
                     }
-                }
-                if (option == 2) {
+                    break;
+
+                case 2:
+                    int max = arrNumber[0];
+                    for (int index = 0; index < arrLength; index++) {
+                        if (arrNumber[index] > max) {
+                            max = arrNumber[index];
+                        }
+                    }
                     System.out.println("The maximum number from array list: \n" + max);
-                } else {
-                    System.out.println("The minimum number from array list: \n " + min);
-                }
-            } else if (option == 4) {
-                System.out.println("Please input a number: \n");
-                int number = scanner.nextInt();
-                boolean isFound = false;
-                for (int index = 0; index < arrLength; index++) {
-                    if (arrNumber[index] == number) {
-                        System.out.printf("The number and its index: \n %d \n %d\n", number, index);
-                        isFound = true;
+                    break;
+
+                case 3:
+                    int min = arrNumber[0];
+                    for (int index = 0; index < arrLength; index++) {
+                        if (arrNumber[index] < min) {
+                            min = arrNumber[index];
+                        }
                     }
-                }
-                if (!isFound) {
-                    System.out.println("Number is not in the list");
-                }
+                    System.out.println("The minimum number from array list: \n " + min);
+                    break;
 
-            } else {
-                System.out.println("Please input the correct option!");
+                case 4:
+                    System.out.println("Please input a number: \n");
+                    int number = scanner.nextInt();
+                    boolean isFound = false;
+                    for (int index = 0; index < arrLength; index++) {
+                        if (arrNumber[index] == number) {
+                            System.out.printf("The number and its index: \n %d \n %d\n", number, index);
+                        }
+                    }
+                    break;
+
+                default:
+                    System.out.println(" This option invalid");
             }
-
         }
         System.out.println("See you again");
     }
